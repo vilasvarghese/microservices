db7928c060dc881d6c127f6a0430c1ed7dd982e8
vilasvarghese <vilas.varghese@gmail.com>
2020-04-24 19:45:14 +0530

    Reverted fault tolerance testing retaining the rest of config.

--- microservices-employee-catalog/src/main/java/com/hsbg/MicroservicesEmployeeCatalogApplication.java
+++ microservices-employee-catalog/src/main/java/com/hsbg/MicroservicesEmployeeCatalogApplication.java
@@ -20,7 +20,7 @@ public class MicroservicesEmployeeCatalogApplication {
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-		factory.setConnectTimeout(500);
+		factory.setConnectTimeout(1000);
 		return new RestTemplate(factory);
 	}
 }
